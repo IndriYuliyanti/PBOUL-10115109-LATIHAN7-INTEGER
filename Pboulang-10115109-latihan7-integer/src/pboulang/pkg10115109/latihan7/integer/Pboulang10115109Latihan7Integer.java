a7a9d317f7154ccc42a753c542d7ec019daeae6e
IndriYuliyanti <55839650+IndriYuliyanti@users.noreply.github.com>
2019-09-26 21:16:54 +0700

    Add files via upload

---
+++ Pboulang-10115109-latihan7-integer/src/pboulang/pkg10115109/latihan7/integer/Pboulang10115109Latihan7Integer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10115109.latihan7.integer;
+
+/**
+ *
+ * @author User
+ */
+public class Pboulang10115109Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 9000000000L;
+        System.out.println("byte :" + b);
+        System.out.println("short :" + s);
+        System.out.println("int :" + i);
+        System.out.println("long :" + l);
+        
+        
+        
+    }
+    
+}
